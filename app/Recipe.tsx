52c9380988a485c223c357348853350351db3184
Kit Cox <chriscox@abandonedfactory.net>
2025-11-03 15:16:28 -0800

    Simple 1-row flowcharts for fertilizer recipes

---
+++ app/Recipe.tsx
@@ -0,0 +1,62 @@
+import './styles.css'
+import {GetStaticProps} from 'next';
+import data from './data.json';
+
+
+export class RecipeProps {
+    public name!: string;
+    public quantity?: number;
+    public workstation?: string;
+    public ingredients?: RecipeProps[];
+}
+
+export class RecipeSetProps {
+    public setName!: string;
+    public recipes!: RecipeProps[];
+}
+
+
+
+export function Recipe(props: RecipeProps) {
+    const renderIngredients = (ingredients: RecipeProps[]) => {
+        return ingredients.map((ingredient, i) => (
+            <>
+                <div className="flowchart-connector"></div>
+                <Recipe key={i} {...ingredient} />
+            </>
+        ))
+    }
+    const renderQty = (props.quantity) ? <span>{props.quantity}x </span> : <span></span>;
+    const renderWorkstation = (props.workstation) ? <h2 className="text-sm">@{props.workstation} </h2> : <span></span>;
+    return (
+        <>
+            <div className="recipe flowchart-node">
+                <h1>{renderQty}{props.name}</h1>
+                {renderWorkstation}
+            </div>
+            {props.ingredients && renderIngredients(props.ingredients)}
+        </>
+    )
+}
+
+
+export function RecipeSet(props: {recipeSets: RecipeSetProps[]}) {
+    const render = props.recipeSets.map((r: RecipeSetProps, idx) => <>
+        <div className="recipeset-container" key={idx}>
+            <h1>{r.setName}</h1>
+            {r.recipes.map((e, i) => <div key={i} className="app flex flex-wrap justify-around items-start flowchart-container">
+                <Recipe key={i} {...e} />
+            </div>)}
+        </div>
+        </>
+    )
+
+    return <>{render}</>
+}
+
+export async function getStaticProps() {
+    // load the recipes from local file data.json
+    // map the recipes into RecipeSetProps where the setName is the key, and the recipes is the value
+    const recipeSets = Object.entries(data).map(([setName, recipes]: [string, RecipeProps[]]) => ({ setName, recipes }));
+    return {props:{recipeSets}}
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    basePath: '/games/gyk',
+    output: "export"
   /* config options here */
 };
-
+module.exports = nextConfig;
 export default nextConfig;
